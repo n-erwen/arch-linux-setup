6fa265bb78ec619f9cf212dc6a4feb175074729f
n-erwen <n.erwen@icloud.com>
2025-10-17 23:31:29 +0100

    Update arch-linux-vmware-install.sh

--- arch-linux-vmware-install.sh
+++ arch-linux-vmware-install.sh
@@ -1,8 +1,8 @@
-loadkeys uk
-setfont ter-132b
+loadkeys uk && \
+setfont ter-132b && \
 ping -c 3 ping.archlinux.org || (echo "No internet connection" && exit 1)
 
-timedatectl set-ntp true
+timedatectl set-ntp true && \
 
 # Create 4GB boot parition and root (/) partition taking up rest of disk space
 fdisk /dev/sda << EOF
@@ -18,31 +18,30 @@ n
 w
 EOF
 
-mkfs.ext4 /dev/sda2
-mkfs.fat -F 32 /dev/sda1
+mkfs.ext4 /dev/sda2 && \
+mkfs.fat -F 32 /dev/sda1 && \
 
-mount /dev/sda2 /mnt
-mkdir /mnt/boot
-mount /dev/sda1 /mnt/boot
+mount /dev/sda2 /mnt && \
+mkdir /mnt/boot && \
+mount /dev/sda1 /mnt/boot && \
 
-pacstrap -K /mnt base linux linux-firmware
+pacstrap -K /mnt base linux linux-firmware && \
 
-genfstab -U /mnt >> /mnt/etc/fstab
-arch-chroot /mnt
+genfstab -U /mnt >> /mnt/etc/fstab && \
+arch-chroot /mnt && \
 
-ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
-hwclock --systohc
+ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime && \
+hwclock --systohc && \
 
 echo "LANG=en_GB.UTF-8" > /etc/locale.conf
 echo "KEYMAP=uk" > /etc/vconsole.conf
 echo "archlinux" > /etc/hostname
 
-mkinitcpio -P
-passwd
+mkinitcpio -P && \
+passwd && \
 
-pacman -S grub efibootmgr
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-grub-mkconfig -o /boot/grub/grub.cfg
-
+pacman -S grub efibootmgr && \
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB && \
+grub-mkconfig -o /boot/grub/grub.cfg && \
 exit
 echo "You can now reboot your system."
